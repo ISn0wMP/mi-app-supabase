c7f89d22e4dc8654952c966c1cdfb481cc24b1c3
ISn0wmp <herreragael672@gmail.com>
2026-05-15 15:19:28 -0600

    Primer commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { createClient }
 from '@supabase/supabase-js'
 
 const supabaseUrl = "https://rwywmafplybrzwaenupc.supabase.co"
-const supabaseKey = "sb_publishable_iNanHaHmKct4qPZK_SfG1A_dnM3v4ME"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3eXdtYWZwbHlicnp3YWVudXBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NTc0MDYsImV4cCI6MjA5NDQzMzQwNn0.hh4XSQppQ2bHPN9v1KYLTneXnqzystr9dOglbohhhzQ"
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
